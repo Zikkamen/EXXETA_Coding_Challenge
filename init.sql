95945d8f3a0b6625bef752d8bf778a13235f6d61
Zikkamen <jeffreyntue@gmail.com>
2024-03-13 10:22:03 +0100

    Added Indexing to sql table

--- init.sql
+++ init.sql
@@ -1,6 +1,7 @@
 CREATE TYPE hotel_room_types AS ENUM('(Einzelzimmer', 'Doppelzimmer,', 'Suite');
 CREATE TABLE IF NOT EXISTS hotel_rooms (
-    ID INT PRIMARY KEY,
+    ID INT UNIQUE PRIMARY KEY,
     hotel_size hotel_room_types,
     has_minibar BOOLEAN
 );
+CREATE INDEX IF NOT EXISTS index_room_type ON hotel_rooms(hotel_size);
